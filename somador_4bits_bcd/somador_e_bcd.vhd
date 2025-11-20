a8f81239b38d65b0c4fbb540443c7b1f28a9333c
JoaoAlbuquerque <joao.albuquerque.704@ufrn.edu.br>
2025-11-20 16:28:25 -0300

    Final 20_11_2025

--- somador_4bits_bcd/somador_e_bcd.vhd
+++ somador_4bits_bcd/somador_e_bcd.vhd
@@ -1,6 +1,6 @@
 -- ELE0518 - LABORATORIO DE SISTEMAS DIGITAIS - T03 (2025.2)
--- Atividade Bônus: Circuito somador que mostra o resultado completo (Unidade e Dezena) Em dois displays de 7 segmentos.
--- Autores: Emanoel Ferreira dos Santos e João Pedro Freire de Albuquerque
+-- Atividade bonus: Circuito somador que mostra o resultado completo (Unidade e Dezena) Em dois displays de 7 segmentos.
+-- Autores: Emanoel Ferreira dos Santos e joao Pedro Freire de Albuquerque
 -- Data 18/11/2025
 
 -- Biblioteca
@@ -15,8 +15,8 @@ entity somador_e_bcd is
         -- Entradas
         num_1, num_2    :   in  std_logic_vector(3 downto 0);
 
-        -- Saída
-        hex_dezena, hex_unidade    :   out std_logic_vector(6 to 0)
+        -- SaÃƒÂ­da
+        hex_dezena, hex_unidade    :   out std_logic_vector(6 downto 0)
     );
 end somador_e_bcd;
 
@@ -27,7 +27,7 @@ architecture top_level of somador_e_bcd is
     signal  cout            :   std_logic;
     -- Merge de sum e cout
     signal  cout_sum        :   unsigned(4 downto 0);
-    signal  dezena, unidade :   std_logic(3 downto 0);
+    signal  dezena, unidade :   std_logic_vector(3 downto 0);
 
     -- Componentes
     
@@ -48,7 +48,7 @@ architecture top_level of somador_e_bcd is
         -- Entrada do decoder
         numero_binario  :   in  std_logic_vector(3 downto 0);
 
-        -- Saída do decoder
+        -- Saida do decoder
         numero_7seg     :   out std_logic_vector(6 downto 0)
     );
     end component bcd;
@@ -58,12 +58,10 @@ architecture top_level of somador_e_bcd is
     somador: carry_ripple_4bit_adder port map(num_1, num_2, '0', sum, cout);
     
     -- Concatenando o bit cout com o vetor da soma
-    cout_sum <= cout & sum;
+    cout_sum <= unsigned(cout & sum);
 
-    -- Calculando a dezena
-    dezena <= ((cout_sum / 10)(3 downto 0));
-    -- Calculando a unidade
-    unidade <= (cout_sum mod 10)(3 downto 0);
+    dezena <= std_logic_vector(to_unsigned(to_integer(cout_sum) / 10, 4));
+	 unidade <= std_logic_vector(to_unsigned(to_integer(cout_sum) mod 10, 4));
 
     -- Instanciando BCDs
     bcd_dezena: bcd port map(dezena, hex_dezena);
